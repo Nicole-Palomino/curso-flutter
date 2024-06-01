818b799976383b0431bfff74f8deb5d9e8dd44b9
Nicole-Palomino <alessa.kz.fx@gmail.com>
2024-05-31 22:06:11 -0500

    Lección 7: Persistencia de datos

--- persistencia_datos/lib/main.dart
+++ persistencia_datos/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Shared Preferences'),
     );
   }
 }
@@ -31,6 +32,32 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
 
+  int value = 0;
+
+  _changeValue() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    
+    setState(() {
+      value += 1;
+      prefs.setInt("value", value);
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    _cargarPreferencias();
+  }
+
+  _cargarPreferencias() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+  
+    setState(() {
+      value = prefs.getInt("value") ?? 0;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -39,7 +66,18 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        child: Text('Hola mundo')
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              child: Text(value.toString(), style: TextStyle(fontSize: 30),)
+            ),
+            ElevatedButton(
+              onPressed: _changeValue, 
+              child: Text('Aumentar valor')
+            )
+          ],
+        )
       ),
     );
   }
